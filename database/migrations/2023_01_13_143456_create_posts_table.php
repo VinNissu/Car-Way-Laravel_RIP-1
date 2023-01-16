88dc9571933c00d6363790423520102d2d6dbb44
Nissu <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2023-01-16 07:54:06 -0300

    Revert "Revert "Initial commit""
    This reverts commit 60b96cb7bc6d112db6f0bb5cd69799e434099a89.

---
+++ database/migrations/2023_01_13_143456_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('midia')->nullable();
+            $table->integer('id_user');
+            $table->text('mensagem');
+            $table->text('localizacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ resources/views/grupo.blade.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tela da comunidade</title>
+  <!-- conection -->
+  <!-- Bibliotecas de icones -->
+  <!--Fonte Awesome-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!--Fonte Awesome-->
+  <!--Recursos do Bootstrapp-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <!--Recursos do Bootstrapp-->
+  <!-- Conexao com css    -->
+  <link rel="stylesheet" href="./css/style.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <!-- Conexao com css    -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Conexao data aos js-->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <!-- Conexao data aos js-->
+
+
+
+  <!-- ---------------------------------------- -->
+</head>
+
+<body>
+  <header class="my-5">
+    <a href="/feed">
+      <button type="button" class="login__button__voltar__feed">
+        <i class="fa-solid fa-arrow-left"></i>     
+      </button>
+    </a>
+    
+    <h2 class="text-center fw-bold text-uppercase fs-1 txt-laranja">
+      Comunidade
+    </h2>
+    <p class="text-center col-md-6 offset-md-3 mb-3">
+      Aqui você encontrará às comunidades cadastradas.
+    </p>
+  </header>
+
+
+  <main class="justify-content-between mx-5">
+    <section>
+      <div class="my-5">
+        <div class="" data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom">
+          <div class="card card__config border-0">
+            <img class="imagem__cardbox" src="imgs/logo-car-way.png" alt="grupo" />
+            <div class="card-body">
+              <h4 class="card-title">Das antigas</h4>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+      </section>
+
+      <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/impact-blue.jpeg" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">Impact Blue</h4>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+
+        <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/emperor.jpeg" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">Emperor</h4>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+    </section>
+  </main>
+  <hr>
+
+    <main class="justify-content-between mx-5">
+    <section>
+      <div class="my-5">
+        <div class="" data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom">
+          <div class="card card__config border-0">
+            <img class="imagem__cardbox" src="imgs/Akina.jpeg" alt="grupo" />
+            <div class="card-body">
+              <h4 class="card-title">Akina</h4>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+      </section>
+
+      <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/NightKids.jpg" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">NightKids</h4>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+
+        <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/red-sus.png" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">Red Suns</h4>
+                  <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+    </section>
+  </main>
+  <hr>
+  <main class="justify-content-between mx-5">
+    <section>
+      <div class="my-5">
+        <div class="" data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom">
+          <div class="card card__config border-0">
+            <img class="imagem__cardbox" src="imgs/Akina.jpeg" alt="grupo" />
+            <div class="card-body">
+              <h4 class="card-title">Akina</h4>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+      </section>
+
+      <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/NightKids.jpg" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">NightKids</h4>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+
+        <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/red-sus.png" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">Red Suns</h4>
+                  <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+    </section>
+  </main>
+  <hr>
+  <main class="justify-content-between mx-5">
+    <section>
+      <div class="my-5">
+        <div class="" data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom">
+          <div class="card card__config border-0">
+            <img class="imagem__cardbox" src="imgs/Akina.jpeg" alt="grupo" />
+            <div class="card-body">
+              <h4 class="card-title">Akina</h4>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+      </section>
+
+      <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/NightKids.jpg" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">NightKids</h4>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+
+        <section>
+        <div class="my-5">
+          <div class="" data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom">
+            <div class="card card__config border-0">
+              <img class="imagem__cardbox" src="imgs/red-sus.png" alt="grupo" />
+              <div class="card-body">
+                <h4 class="card-title">Red Suns</h4>
+                  <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro quidem seq.
+        </section>
+    </section>
+  </main>
+  
+
+</body>
+
+<script src="js/costumer.js"></script>
+
+</html>
